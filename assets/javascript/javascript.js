ac3c12cf0bea59b4e10a896f7b87f8f0e9ce34e8
Christopher Elliott <christopher.elliott8@gmail.com>
2017-08-12 21:32:13 -0500

    Commit #2

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,14 @@
+window.onload = rotate;
+
+var theAd = 0;
+var zmbImages = new Array("assets/images/zmb1.png", "assets/images/zmb2.png", "assets/images/zmb3.png", "assets/images/zmb4.png", "assets/images/zmb5.png");
+
+function rotate() {
+    theAd++;
+    if (theAd == zmbImages.length) {
+        theAd = 0;
+    }
+    document.getElementById("zomBies").src = zmbImages[theAd];
+
+    setTimeout(rotate, 3 * 1000);
+}
